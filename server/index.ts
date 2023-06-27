f6749f824afae0eb007cb08c6dd8da1a42463064
Azat S <to@azat.io>
2023-06-27 13:47:09 +0300

    chore: update eslint config

--- server/index.ts
+++ server/index.ts
@@ -3,14 +3,14 @@ import type { Plugin } from '@vuepress/core'
 import { getDirname, path } from '@vuepress/utils'
 
 interface UmamiAnalyticsPluginOptions {
-  id: string
   src: string
+  id: string
 }
 
 let __dirname = getDirname(import.meta.url)
 
 export let umamiAnalyticsPlugin =
-  ({ id, src }: UmamiAnalyticsPluginOptions): Plugin =>
+  ({ src, id }: UmamiAnalyticsPluginOptions): Plugin =>
   app => {
     let plugin = {
       name: 'vuepress-plugin-umami-analytics',
@@ -22,10 +22,10 @@ export let umamiAnalyticsPlugin =
 
     return {
       ...plugin,
-      clientConfigFile: path.resolve(__dirname, '../client/index.js'),
       define: {
-        __UMAMI_ANALYTICS_ID__: id,
         __UMAMI_ANALYTICS_SRC__: src,
+        __UMAMI_ANALYTICS_ID__: id,
       },
+      clientConfigFile: path.resolve(__dirname, '../client/index.js'),
     }
   }
