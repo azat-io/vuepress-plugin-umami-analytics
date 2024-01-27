8fcdd193dd9e05e8e7bd9ee8ca0a0976113202e6
Lucien Cartier-Tilet <lucien@phundrak.com>
2024-01-27 18:11:46 +0100

    feat: enable tracker configuration

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,11 @@ import type { Plugin } from '@vuepress/core'
 import { getDirname, path } from '@vuepress/utils'
 
 interface UmamiAnalyticsPluginOptions {
+  doNotTrack?: boolean
+  autoTrack?: boolean
+  domains?: string[]
+  hostUrl?: string
+  cache?: boolean
   src: string
   id: string
 }
@@ -10,7 +15,15 @@ interface UmamiAnalyticsPluginOptions {
 let __dirname = getDirname(import.meta.url)
 
 export let umamiAnalyticsPlugin =
-  ({ src, id }: UmamiAnalyticsPluginOptions): Plugin =>
+  ({
+    doNotTrack,
+    autoTrack,
+    hostUrl,
+    domains,
+    cache,
+    src,
+    id,
+  }: UmamiAnalyticsPluginOptions): Plugin =>
   app => {
     let plugin = {
       name: 'vuepress-plugin-umami-analytics',
@@ -23,6 +36,11 @@ export let umamiAnalyticsPlugin =
     return {
       ...plugin,
       define: {
+        __UMAMI_ANALYTICS_DO_NOT_TRACK__: doNotTrack,
+        __UMAMI_ANALYTICS_AUTO_TRACK__: autoTrack,
+        __UMAMI_ANALYTICS_HOST_URL__: hostUrl,
+        __UMAMI_ANALYTICS_DOMAINS__: domains,
+        __UMAMI_ANALYTICS_CACHE__: cache,
         __UMAMI_ANALYTICS_SRC__: src,
         __UMAMI_ANALYTICS_ID__: id,
       },
