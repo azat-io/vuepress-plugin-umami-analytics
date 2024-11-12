341bd4b186161c059378a524da07519202b27bbb
Azat S. <to@azat.io>
2024-11-12 23:03:38 +0300

    refactor: improve code quality

--- client/index.ts
+++ client/index.ts
@@ -17,31 +17,22 @@ export default defineClientConfig({
     let scriptTag = document.createElement('script')
     scriptTag.async = true
     scriptTag.src = __UMAMI_ANALYTICS_SRC__
-    scriptTag.setAttribute('data-website-id', __UMAMI_ANALYTICS_ID__)
+    scriptTag.dataset.websiteId = __UMAMI_ANALYTICS_ID__
     if (__UMAMI_ANALYTICS_HOST_URL__) {
-      scriptTag.setAttribute('data-host-url', __UMAMI_ANALYTICS_HOST_URL__)
+      scriptTag.dataset.hostUrl = __UMAMI_ANALYTICS_HOST_URL__
     }
     if (__UMAMI_ANALYTICS_AUTO_TRACK__) {
-      scriptTag.setAttribute(
-        'data-auto-track',
-        '' + __UMAMI_ANALYTICS_AUTO_TRACK__,
-      )
+      scriptTag.dataset.autoTrack = `${__UMAMI_ANALYTICS_AUTO_TRACK__}`
     }
     if (__UMAMI_ANALYTICS_DO_NOT_TRACK__) {
-      scriptTag.setAttribute(
-        'data-do-not-track',
-        '' + __UMAMI_ANALYTICS_DO_NOT_TRACK__,
-      )
+      scriptTag.dataset.doNotTrack = `${__UMAMI_ANALYTICS_DO_NOT_TRACK__}`
     }
     if (__UMAMI_ANALYTICS_CACHE__) {
-      scriptTag.setAttribute('data-cache', '' + __UMAMI_ANALYTICS_CACHE__)
+      scriptTag.dataset.cache = `${__UMAMI_ANALYTICS_CACHE__}`
     }
     if (__UMAMI_ANALYTICS_DOMAINS__) {
-      scriptTag.setAttribute(
-        'data-domains',
-        __UMAMI_ANALYTICS_DOMAINS__.join(','),
-      )
+      scriptTag.dataset.domains = __UMAMI_ANALYTICS_DOMAINS__.join(',')
     }
-    document.body.appendChild(scriptTag)
+    document.body.append(scriptTag)
   },
 })
